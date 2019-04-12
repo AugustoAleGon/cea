1ff3b135ce7363760c2dece7dba64c6719aa8f79
Daniel Idarraga <idarraga.daniel@gmail.com>
2019-04-12 11:46:59 -0500

    agregue url nueva para imagen por defecto

--- models/noticias.js
+++ models/noticias.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const NoticiasSchema = new mongoose.Schema({
     imagen: {
         type: String,
-        default: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjE7obL18HhAhWCzlkKHTt2AisQjRx6BAgBEAU&url=https%3A%2F%2Fwww.wsls.com%2Ftop-stories%2Fwhats-news-today-february-18-2019&psig=AOvVaw0iiFWq31BpRsUNkGCRxh3F&ust=1554853710449155.png"
+        default: "https://https://lamenteesmaravillosa.com/wp-content/uploads/2017/11/libro-con-un-arbol.jpg.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjE7obL18HhAhWCzlkKHTt2AisQjRx6BAgBEAU&url=https%3A%2F%2Fwww.wsls.com%2Ftop-stories%2Fwhats-news-today-february-18-2019&psig=AOvVaw0iiFWq31BpRsUNkGCRxh3F&ust=1554853710449155.png"
     },
 
     titulo: {
